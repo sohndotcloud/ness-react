473f2cc8fa022b0abb15a4a5aa2731738cf3b331
Nils Sohn <sohn.cloud@proton.me>
2026-07-27 17:52:36 -0700

    Navigating to home after login

--- app/page/login-form.tsx
+++ app/page/login-form.tsx
@@ -1,18 +1,20 @@
 import { useState } from "react";
 import type { FormEvent } from "react";
 import { useAuth } from "../context/AuthContext";
+import {useNavigate} from "react-router";
 
 function LoginForm() {
     const { login } = useAuth();
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [error, setError] = useState("");
+    const navigate = useNavigate();
 
     const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         try {
             await login(email, password);
-            // redirect on success, e.g. navigate("/dashboard")
+            navigate("/");
         } catch (err) {
             setError("Invalid email or password");
         }
